27f124e47f1f3f2e401cab9f2b2f830f4565870f
The Hoang <phanthehoang2503@gmail.com>
2025-10-15 12:00:44 +0700

    fixed some file related issues

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go_training_ground
+module github.com/phanthehoang2503/go_training_ground
 
 go 1.25.1
--- linkedList/main.go
+++ linkedList/main.go
@@ -1,7 +1,9 @@
 package main
 
+import "github.com/phanthehoang2503/go_training_ground/linkedList/dsa"
+
 func main() {
-	list := LinkedList{}
+	list := dsa.LinkedList{}
 	list.Print()
 	list.Insert(10)
 	list.Insert(20)
